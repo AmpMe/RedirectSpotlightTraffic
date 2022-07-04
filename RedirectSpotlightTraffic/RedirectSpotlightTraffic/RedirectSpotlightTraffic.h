d37fd300515d40d0bd925364e3a20b32eaf8d924
Shamari Ishmael <shamariishmael@gmail.com>
2022-07-04 12:58:13 -0600

    Initial commit

---
+++ RedirectSpotlightTraffic/RedirectSpotlightTraffic/RedirectSpotlightTraffic.h
@@ -0,0 +1,18 @@
+//
+//  RedirectSpotlightTraffic.h
+//  RedirectSpotlightTraffic
+//
+//  Created by Shamari Ishmael on 7/4/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RedirectSpotlightTraffic.
+FOUNDATION_EXPORT double RedirectSpotlightTrafficVersionNumber;
+
+//! Project version string for RedirectSpotlightTraffic.
+FOUNDATION_EXPORT const unsigned char RedirectSpotlightTrafficVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RedirectSpotlightTraffic/PublicHeader.h>
+
+
---
+++ RedirectSpotlightTraffic/RedirectSpotlightTraffic/RedirectSpotlightTrafficSingleton.swift
@@ -0,0 +1,117 @@
+//
+//  RedirectSpotlightTrafficSingleton.swift
+//  RedirectSpotlightTraffic
+//
+//  Created by Shamari Ishmael on 7/4/22.
+//
+
+import Foundation
+import CoreSpotlight
+import CoreServices
+import UIKit
+import UniformTypeIdentifiers
+
+public struct SpotlightRedirectConfig {
+    var filePath: String
+    
+    public init(filePath: String) {
+        self.filePath = filePath
+    }
+}
+
+public class RedirectSpotlightTrafficSingleton {
+    public static let shared = RedirectSpotlightTrafficSingleton()
+    
+    public static var config: SpotlightRedirectConfig?
+    
+    public class func setup(_ config: SpotlightRedirectConfig) {
+        RedirectSpotlightTrafficSingleton.config = config
+        
+        RedirectSpotlightTrafficSingleton.shared.indexKeywordsInBackground()
+    }
+    
+    public init() {
+        guard let _ = RedirectSpotlightTrafficSingleton.config else {
+            fatalError("Error - you must call setup before accessing RedirectSpotlightTrafficSingleton.shared")
+        }
+        print("Successfully Initialized RedirectSpotlightTrafficSingleton")
+    }
+    
+    
+    
+    private func indexKeywords() {
+        // Delete index first
+        CSSearchableIndex.default().deleteAllSearchableItems { error in
+            if error != nil {
+                print(error?.localizedDescription ?? "error deleting all searchable items")
+            } else {
+                // At this stage the index has been deleted
+                var searchableItems: [CSSearchableItem] = []
+                
+                guard let filePath = RedirectSpotlightTrafficSingleton.config?.filePath else {
+                    return
+                }
+                
+                var csvData = ""
+                do {
+                    csvData = try String(contentsOfFile: filePath)
+                } catch {
+                    print(error)
+                    return
+                }
+                
+                var rows = csvData.components(separatedBy: "\n")
+                
+                rows.removeFirst()
+                
+                for row in rows {
+                    let columns = row.components(separatedBy: ",")
+                    
+                    if columns.count == 3 {
+                        let spotlightTerm = columns[0]
+                        let searchTerm = columns[1]
+                        let country = columns[2].replacingOccurrences(of: "\r", with: "")
+                        
+                        let deviceCountry = Locale.current.regionCode
+                        
+                        if deviceCountry?.uppercased() == country.uppercased() {
+                            let attributeSet = CSSearchableItemAttributeSet(contentType: .text)
+                            
+                            attributeSet.title = spotlightTerm
+                            attributeSet.contentDescription = searchTerm
+                            attributeSet.country = country
+                            
+                            let item = CSSearchableItem(uniqueIdentifier: searchTerm, domainIdentifier: nil, attributeSet: attributeSet)
+                            searchableItems.append(item)
+                        }
+                    }
+                }
+                
+                NSLog("Spotlight: Indexing Started for \(searchableItems.count) items")
+                CSSearchableIndex.default().indexSearchableItems(searchableItems) { error in
+                    if error != nil {
+                        NSLog(error?.localizedDescription ?? "error indexing searchable items")
+                    } else {
+                        // Indexing finish
+                        NSLog("Spotlight: Indexing Finished for \(searchableItems.count) items")
+                    }
+                }
+            }
+        }
+    }
+    
+    public func indexKeywordsInBackground() {
+        DispatchQueue.global(qos: .background).async {
+            self.indexKeywords()
+        }
+    }
+    
+    public func application(continue userActivity: NSUserActivity) {
+        if userActivity.activityType == CSSearchableItemActionType {
+            if let uniqueIdentifier = userActivity.userInfo?[CSSearchableItemActivityIdentifier] as? String,
+               let url = URL(string: "https://www.bing.com/search?q=\(uniqueIdentifier)") {
+                UIApplication.shared.open(url)
+            }
+        }
+    }
+}
